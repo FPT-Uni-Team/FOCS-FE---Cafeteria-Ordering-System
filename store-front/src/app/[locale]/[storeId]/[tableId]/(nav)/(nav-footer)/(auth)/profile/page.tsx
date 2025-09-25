4ba84d827688c55263351db922bbf6ac559f9425
Cuongnc165 <cuongnc0503@gmail.com>
2025-09-26 00:29:06 +0700

    fix/update-profile

--- store-front/src/app/[locale]/[storeId]/[tableId]/(nav)/(nav-footer)/(auth)/profile/page.tsx
+++ store-front/src/app/[locale]/[storeId]/[tableId]/(nav)/(nav-footer)/(auth)/profile/page.tsx
@@ -4,6 +4,7 @@ import { IAuthenticationProps } from "@/types/common";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/auth/authOptions";
 import userService from "@/services/userService";
+import cartService from "@/services/cartService";
 
 export async function generateMetadata(props: IAuthenticationProps) {
   const { locale } = await props.params;
@@ -22,10 +23,12 @@ export default async function ProfilePage() {
   const session = await getServerSession(authOptions);
   const token = session?.accessToken;
   const res = await userService.detail(token);
+  const resPoints = await cartService.get_point();
   const user = {
     fullName: `${res.data.first_name} ${res.data.last_name}`,
     email: res.data.email,
     avatar: res.data.avatar || "/img/profile/default-avatar.jpg",
+    point: resPoints.data.point || 0,
   };
 
   return <Profile user={user} />;
